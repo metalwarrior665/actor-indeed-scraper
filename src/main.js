1e14d1cc4aca3616f09108075f11a9e8217417a8
olegVeselov92 <oleg.veselov92@gmail.com>
2021-06-30 22:47:22 +0300

    fixed pagination, fixed selectors, implemented better logging

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 const Apify = require('apify');
 const urlParse = require('url-parse');
 
+const {
+    log
+} = Apify.utils;
+
 function makeUrlFull(href, urlParsed) {
     if (href.substr(0, 1) === '/') return urlParsed.origin + href;
     return href;
 }
 
 function getIdFromUrl(url) {
-    console.log(url);
     return (url.match(new RegExp('(?<=jk=).*?$')) ? url.match(new RegExp('(?<=jk=).*?$'))[0] : '');
 }
 
+// ADD URLS FROM INPUT
 const fromStartUrls = async function* (startUrls, name = 'STARTURLS') {
     const rl = await Apify.openRequestList(name, startUrls);
-
     /** @type {Apify.Request | null} */
     let rq;
-
     // eslint-disable-next-line no-cond-assign
     while (rq = await rl.fetchNextRequest()) {
         yield rq;
@@ -33,9 +35,16 @@ Apify.main(async () => {
         startUrls,
         maxItems,
         extendOutputFunction,
-        proxyConfiguration = { useApifyProxy: true },
+        proxyConfiguration = {
+            useApifyProxy: true
+        },
     } = input;
 
+
+    if (maxItems > 990) {
+        log.warn(`The limit of items you set exceeds maximum allowed value. Max possible number of offers, that can be processed is 990.`)
+    }
+    // EXTENDED FUNCTION FROM INPUT
     let extendOutputFunctionValid;
     if (extendOutputFunction) {
         try {
@@ -60,33 +69,38 @@ Apify.main(async () => {
         nl: 'https://www.indeed.nl',
         za: 'https://www.indeed.co.za',
     };
-
     const countryUrl = countryDict[country.toLowerCase()] || `https://${country || 'www'}.indeed.com`;
+    // COUNTER OF ITEMS TO SAVE 
+    let itemsCounter = 0;
 
     const requestQueue = await Apify.openRequestQueue();
-
-
-    // Using startUrls disables search
+    // IF THERE ARE START URLS => ADDING THEM TO THE QUEUE
+    // Using startUrls => disables search
     if (Array.isArray(startUrls) && startUrls.length > 0) {
-      for await (const req of fromStartUrls(startUrls)) { // this line changed
-          if (!req.url) throw 'StartURL in bad format, needs to be object with url field';
-          if (!req.userData) req.userData = {};
-          if (!req.userData.label) req.userData.label = 'START';
-          if (req.url.includes("viewjob")) req.userData.label = 'DETAIL'
-          await requestQueue.addRequest(req);
-          console.log(`This url will be scraped: ${req.url}`);
-            }
-        
-    } else {
-        console.log(`Running site crawl country ${country}, position ${position}, location ${location}`);
+        for await (const req of fromStartUrls(startUrls)) { // this line changed
+            if (!req.url) throw 'StartURL in bad format, needs to be object with url field';
+            if (!req.userData) req.userData = {};
+            if (!req.userData.label) req.userData.label = 'START';
+            req.userData.itemsCounter = itemsCounter;
+            if (req.url.includes("viewjob")) req.userData.label = 'DETAIL'
+            await requestQueue.addRequest(req);
+            log.info(`This url will be scraped: ${req.url}`);
+        }
 
+    }
+    // IF NO START URL => CREATING FIRST "LIST"  PAGE ON OUR OWN
+    else {
+        log.info(`Running site crawl country ${country}, position ${position}, location ${location}`);
         const startUrl = `${countryUrl}/jobs?${position ? `q=${encodeURIComponent(position)}&` : ''}${location ? `l=${encodeURIComponent(location)}` : ''}`;
-
-        await requestQueue.addRequest({ url: startUrl, userData: { label: 'START' } });
+        await requestQueue.addRequest({
+            url: startUrl,
+            userData: {
+                label: 'START',
+                itemsCounter: itemsCounter,
+            }
+        });
     }
 
-
-    let counter = 0;
     const sdkProxyConfiguration = await Apify.createProxyConfiguration(proxyConfiguration);
 
     // You must use proxy on the platform
@@ -94,7 +108,7 @@ Apify.main(async () => {
         throw 'You must use Apify Proxy or custom proxies to run this scraper on the platform!';
     }
 
-    console.log('starting crawler');
+    log.info('Starting crawler...');
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
         useSessionPool: true,
@@ -105,11 +119,10 @@ Apify.main(async () => {
             },
         },
         maxConcurrency,
-        maxRequestRetries: 10,
+        maxRequestRetries: 15,
         proxyConfiguration: sdkProxyConfiguration,
         handlePageFunction: async ({ $, request, session, response }) => {
-            console.log('url :', request.url);
-            console.log('label :', request.userData.label);
+            log.info(`Label(Page type): ${request.userData.label} || URL: ${request.url}`);
             const urlParsed = urlParse(request.url);
 
             if (![200, 404].includes(response.statusCode)) {
@@ -121,16 +134,33 @@ Apify.main(async () => {
             switch (request.userData.label) {
                 case 'START':
                 case 'LIST':
-                    const details = $('a[data-tn-element="jobTitle"]').get().map((el) => { return { url: makeUrlFull(el.attribs.href, urlParsed), userData: { label: 'DETAIL' } }; });
+                    let itemsCounter = request.userData.itemsCounter;
+                    log.info(`Number of processed offers: ${itemsCounter}`);
+
+                    const details = $('.tapItem').get().map((el) => {
+                        return {
+                            url: makeUrlFull(el.attribs.href, urlParsed),
+                            userData: {
+                                label: 'DETAIL'
+                            }
+                        };
+                    });
+
                     for (const req of details) {
-                        if (!(maxItems && counter >= maxItems)) await requestQueue.addRequest(req);
-                        counter += 1;
+                        if (!(maxItems && itemsCounter >= maxItems) && itemsCounter < 990) await requestQueue.addRequest(req);
+                        itemsCounter += 1;
                     }
 
-                    const lists = $('div[class="pagination"] a').get().map((el) => { return { url: makeUrlFull(el.attribs.href, urlParsed), userData: { label: 'LIST' } }; });
-                    for (const req of lists) {
-                        if (!(maxItems && counter > maxItems)) await requestQueue.addRequest(req);
-                    }
+                    const nextPage = $('a[aria-label="Next"]').attr('href');
+                    const nextPageUrl = {
+                        url: makeUrlFull(nextPage, urlParsed),
+                        userData: {
+                            label: 'LIST',
+                            itemsCounter: itemsCounter,
+                        }
+                    };
+
+                    if (!(maxItems && itemsCounter > maxItems) && itemsCounter < 990) await requestQueue.addRequest(nextPageUrl);
 
                     break;
                 case 'DETAIL':
@@ -138,7 +168,7 @@ Apify.main(async () => {
                         positionName: $('.jobsearch-JobInfoHeader-title').text().trim(),
                         company: $(".jobsearch-JobInfoHeader-subtitle > div > div").eq(0).text(),
                         location: $(".jobsearch-JobInfoHeader-subtitle > div").eq(1).text(),
-                        reviews: $(".jobsearch-JobInfoHeader-subtitle > div > div").eq(1).text().replace(/\D/g,''),
+                        reviews: $(".jobsearch-JobInfoHeader-subtitle > div > div").eq(1).text().replace(/\D/g, ''),
                         url: request.url,
                         id: getIdFromUrl($('meta[id="indeed-share-url"]').attr('content')),
                         description: $('div[id="jobDescriptionText"]').text(),
@@ -149,7 +179,7 @@ Apify.main(async () => {
                             const userResult = await extendOutputFunctionValid($);
                             result = Object.assign(result, userResult);
                         } catch (e) {
-                            console.log('Error in the extendedOutputFunction run', e);
+                            log.info('Error in the extendedOutputFunction run', e);
                         }
                     }
 
@@ -163,5 +193,5 @@ Apify.main(async () => {
     });
     await crawler.run();
 
-    console.log('Done.');
+    log.info('Done.');
 });
